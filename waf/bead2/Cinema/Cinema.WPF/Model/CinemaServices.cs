3a0cbf0d1ba6653048cda8c0fd4593442f75f353
BenJoey <bauerbence5@gmail.com>
2019-05-09 15:24:28 +0200

    WAF|Init models for wpf

---
+++ waf/bead2/Cinema/Cinema.WPF/Model/CinemaServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.WPF.Model
+{
+    public class CinemaServices
+    {
+        private readonly HttpClient _client;
+
+        public bool IsUserLoggedIn;
+
+        public CinemaServices(string baseAddress)
+        {
+            IsUserLoggedIn = false;
+            _client = new HttpClient { BaseAddress = new Uri(baseAddress) };
+        }
+    }
+}
