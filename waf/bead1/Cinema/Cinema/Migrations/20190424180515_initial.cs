2f8f6730dfe02096c716db25ba981ee35e7680d3
BenJoey <bauerbence5@gmail.com>
2019-05-09 13:37:24 +0200

    WAF|Fixed gitignore & added migrations

---
+++ waf/bead1/Cinema/Cinema/Migrations/20190424180515_initial.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cinema.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FullName = table.Column<string>(nullable: true),
+                    UserName = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    PosterPath = table.Column<string>(nullable: true),
+                    Director = table.Column<string>(nullable: true),
+                    Length = table.Column<TimeSpan>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Modified = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RoomName = table.Column<string>(nullable: true),
+                    NumOfRows = table.Column<int>(nullable: false),
+                    NumOfCols = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shows",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Movies = table.Column<int>(nullable: true),
+                    StartTime = table.Column<DateTime>(nullable: false),
+                    RoomRefId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shows", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Shows_Movies_Movies",
+                        column: x => x.Movies,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Shows_Rooms_RoomRefId",
+                        column: x => x.RoomRefId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ShowRefId = table.Column<int>(nullable: false),
+                    Rooms = table.Column<int>(nullable: true),
+                    Row = table.Column<int>(nullable: false),
+                    Col = table.Column<int>(nullable: false),
+                    State = table.Column<int>(nullable: false),
+                    NameReserved = table.Column<string>(nullable: true),
+                    PhoneNum = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Seats_Rooms_Rooms",
+                        column: x => x.Rooms,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Seats_Shows_ShowRefId",
+                        column: x => x.ShowRefId,
+                        principalTable: "Shows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seats_Rooms",
+                table: "Seats",
+                column: "Rooms");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seats_ShowRefId",
+                table: "Seats",
+                column: "ShowRefId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shows_Movies",
+                table: "Shows",
+                column: "Movies");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shows_RoomRefId",
+                table: "Shows",
+                column: "RoomRefId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Seats");
+
+            migrationBuilder.DropTable(
+                name: "Shows");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
