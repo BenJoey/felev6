24db9fec2b04e21fbb3a808ab0320e9faf4cdd7f
BenJoey <bauerbence5@gmail.com>
2019-05-09 15:49:04 +0200

    WAF|Added login window

--- waf/bead2/Cinema/Cinema.WPF/Model/CinemaServices.cs
+++ waf/bead2/Cinema/Cinema.WPF/Model/CinemaServices.cs
@@ -1,22 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Cinema.Persistence.DTOs;
+
 
 namespace Cinema.WPF.Model
 {
-    public class CinemaServices
+    public class CinemaServices : ICinemaService
     {
         private readonly HttpClient _client;
 
-        public bool IsUserLoggedIn;
+        private bool _isUserLoggedIn;
+
+        public bool IsUserLoggedIn => _isUserLoggedIn;
 
         public CinemaServices(string baseAddress)
         {
-            IsUserLoggedIn = false;
+            _isUserLoggedIn = false;
             _client = new HttpClient { BaseAddress = new Uri(baseAddress) };
         }
+
+        public async Task<bool> LoginAsync(string name, string password)
+        {
+            EmployeeDto user = new EmployeeDto() { Username = name, Password = password };
+            HttpResponseMessage res = await _client.PostAsJsonAsync("api/Account/Login", user);
+
+            if (res.IsSuccessStatusCode)
+            {
+                _isUserLoggedIn = true;
+                return true;
+            }
+
+            if (res.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                return false;
+            }
+
+            throw new NetworkException("Service returned response" + res.StatusCode);
+        }
+
     }
 }
---
+++ waf/bead2/Cinema/Cinema.WPF/ViewModel/LoginViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Controls;
+using Cinema.WPF.Model;
+using Cinema.Persistence;
+using Cinema.Persistence.DTOs;
+
+namespace Cinema.WPF.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private readonly ICinemaService _model;
+
+        public DelegateCommand ExitCommand { get; set; }
+        public DelegateCommand LoginCommand { get; set; }
+
+        public string UserName { get; set; }
+
+        public event EventHandler ExitApplication;
+        public event EventHandler LoginSuccess;
+        public event EventHandler LoginFailed;
+
+        public LoginViewModel(ICinemaService model)
+        {
+            this._model = model ?? throw new ArgumentNullException("model");
+            UserName = String.Empty;
+
+            ExitCommand = new DelegateCommand(param => OnExitApplication());
+
+            LoginCommand = new DelegateCommand(param => LoginAsync(param as PasswordBox));
+        }
+
+        private async void LoginAsync(PasswordBox passwordBox)
+        {
+            if (passwordBox == null)
+                return;
+
+            try
+            {
+                bool result = await _model.LoginAsync(UserName, passwordBox.Password);
+
+                if (result)
+                    OnLoginSuccess();
+                else
+                    OnLoginFailed();
+            }
+            catch (NetworkException ex)
+            {
+                OnMessageApplication($"Unexpected error! ({ex.Message})");
+            }
+        }
+        
+        private void OnLoginSuccess()
+        {
+            LoginSuccess?.Invoke(this, EventArgs.Empty);
+        }
+        
+        private void OnExitApplication()
+        {
+            ExitApplication?.Invoke(this, EventArgs.Empty);
+        }
+        
+        private void OnLoginFailed()
+        {
+            LoginFailed?.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+}
